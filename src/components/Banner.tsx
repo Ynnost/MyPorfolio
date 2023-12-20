c3d5bbf68c7210f635ef80da638847750792707d
Ynnost <ynnost94@gmail.com>
2023-12-21 01:16:46 +0300

    commit skills

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,47 @@
-import { Col, Container, Nav, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import { useEffect, useState } from "react";
 
 type BannerType = {};
 
 export const Banner: React.FC<BannerType> = ({}) => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => tick(), delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fulltext = toRotate[i];
+    let updatedText = isDeleting ? fulltext.substring(0, text.length - 1) : fulltext.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updatedText === fulltext) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -12,12 +49,18 @@ export const Banner: React.FC<BannerType> = ({}) => {
           <Col xs={12} md={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm webdecoded`} <span className="wrap">web developer</span>
+              {`Hi I'm webdecoded   `} <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
               text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
+            <button onClick={() => console.log("connect")}>
+              Let's connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Headder Img" />
           </Col>
         </Row>
       </Container>
